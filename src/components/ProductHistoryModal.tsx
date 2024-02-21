de662ed55d309e20e902fe3268716bf0aaa79817
sossut <mikki.alakari@elisanet.fi>
2024-02-21 14:55:45 +0200

    Add history table to ProductHistoryModal

--- src/components/ProductHistoryModal.tsx
+++ src/components/ProductHistoryModal.tsx
@@ -7,13 +7,33 @@ export interface ProductHistoryModalProps {
 }
 
 const ProductHistoryModal: React.FC<ProductHistoryModalProps> = ({onClose, product}) => {
+  const [history, setHistory] = React.useState([])
   return (
     <div className='modal product-history-modal'>
         <button className='close' onClick={onClose}>Sulje</button>
         <h2>Tuotteen historiatiedot</h2>
         <p>Tuote: {product?.code} {product?.name}</p>
         <div className='modal-content product-history-modal-content'>
-        
+            <table>
+                <thead>
+                    <tr>
+                        <th>Lähti/Saapui</th>
+                        <th>Päivämäärä</th>
+                        <th>Määrä</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {history.map((h, i: number) => (
+                        <tr key={i}>
+                            <td>{h.type}</td>
+                            <td>{h.date}</td>
+                            <td>{h.amount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+
+            </table>
         </div>
     </div>
   )
